6fcf6a7073aea8b4b7044452c7110215fda9d95a
dev <devesh1245sharma@gmail.com>
2025-11-15 22:08:21 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import './index.css'
 import React from "react";
-import FoodDeliveryHome from './Pages/Home';
+import FoodDeliveryHome from './Pages/home/Home';
 
 function App() {
 
---
+++ src/Pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Hero from "../../Components/maincomponents/Hero";
+import Categories from "../../Components/maincomponents/Categories";
+import FeaturedHeader from "../../Components/maincomponents/FeaturedHeader";
+import RestaurantGrid from "../../Components/maincomponents/RestaurantGrid";
+import Stats from "../../Components/maincomponents/Stats";
+import Navbar from "../../Components/maincomponents/Navbar";
+import Footer from "../../Components/maincomponents/Footer";
+
+export default function FoodDeliveryHome() {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-slate-50 via-purple-50 to-pink-50">
+      <Navbar />
+      <Hero />
+      <Categories />
+      <FeaturedHeader />
+      <RestaurantGrid />
+      <Stats />
+      <Footer/>
+    </div>
+  );
+}
